0b5ded1ed61fde66f0ee36921278e3f5b4be2693
RussRussBus <russell.hong@icloud.com>
2026-04-19 13:29:29 -0700

    up

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    // hello here too
+    /* how about this*/ printf("hello world\n")/*yo*/; // does this work
+    /*
+    hello here
+    */
+}
+/* yo whats good */
